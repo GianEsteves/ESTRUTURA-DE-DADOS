6a6365740ca680e5abd7bb924ccd256bd432a761
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-03-29 10:48:17 -0300

    criação da classe main

--- Revisao/Main.java
+++ Revisao/Main.java
@@ -1,1 +1,18 @@
-aa
+package Revisao;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Criando dois pontos
+        PC ponto1 = new PC(5.5, 13.7);
+        PC ponto2 = new PC(2.8, 7.9);
+        
+        
+        // Calculando a distância
+        double distancia = ponto1.distanciaEuclidiana(ponto2);
+        
+        // print do resultado
+        System.out.println("A distância entre os pontos (" + ponto1.getA() + ", " + ponto1.getB() + ") e (" + ponto2.getA() + ", " + ponto2.getB() + ") é " + distancia);
+    }
+}

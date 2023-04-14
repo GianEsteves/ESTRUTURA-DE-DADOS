16d3f2c31f89f7838e4e7b3416ccb188badd9c3c
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-04-14 17:14:13 -0300

    Craição das classes da ativade 5

---
+++ Atividade 5/Noh.java
@@ -0,0 +1,19 @@
+public class Noh {
+    private Object info;
+    private Noh next;
+    
+    public Noh (Object info){
+    this.info = info;
+    this.next = null;
+    }
+    
+    public Object getInfo() {
+        return this.info;
+    }
+    
+    public Noh getNext() {
+        return this.next;
+    }
+    
+    public Noh setNext(Noh n) { return this.next = n; }
+}

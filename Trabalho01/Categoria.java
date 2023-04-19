47a5ac3884257291bb0d43fb3ec97228bd160c4f
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-04-19 19:58:52 -0300

    criação das classes

---
+++ Trabalho01/Categoria.java
@@ -0,0 +1,20 @@
+package Trabalho01;
+
+public class Categoria {
+    private String nome;
+    private int indentificador;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIndentificador() {
+        return indentificador;
+    }
+    public void setIndentificador(int indentificador) {
+        this.indentificador = indentificador;
+    }
+    
+}
---
+++ Trabalho01/Cliente.java
@@ -0,0 +1,35 @@
+package Trabalho01;
+
+public class Cliente {
+    private String nome;
+    private long cnh;
+    private long telefone;
+    private long cpf;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public long getCnh() {
+        return cnh;
+    }
+    public void setCnh(long cnh) {
+        this.cnh = cnh;
+    }
+    public long getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+    public long getCpf() {
+        return cpf;
+    }
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+    
+
+}

999e51c219e3d5bac02aaea7910b2ce1eeec5959
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-04-28 21:40:37 -0300

    .

---
+++ Trabalho01/ControlerMenuInicial.java
@@ -0,0 +1,20 @@
+package Trabalho01;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControlerMenuInicial {
+
+    @FXML
+    private Button menuCategoria;
+
+    @FXML
+    private Button menuCliente;
+
+    @FXML
+    private Button menuLocacao;
+
+    @FXML
+    private Button menuVeiculo;
+
+}

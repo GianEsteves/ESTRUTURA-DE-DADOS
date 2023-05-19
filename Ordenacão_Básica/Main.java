0d6dfd53bfad91531d579ef35902e24a6d03e3e8
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-19 17:35:48 -0300

    aula 12, falta medir o tempo

---
+++ Ordenacão_Básica/Main.java
@@ -0,0 +1,14 @@
+package Ordenacão_Básica;
+
+public class Main {
+    public static void main(String[] args){
+        Vetor v = new Vetor();
+        //v.gerarVetorAleatorio();
+        //v.exibirvetororAleatorio();
+        //v.bubble_sort();
+        System.out.println("==============================================");
+        //v.selection_sort();
+        System.out.println("==============================================");
+        //v.insertion_sort();
+    }
+}

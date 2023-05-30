e73c09a98b98e98f306c4ef8af4787a91b2bf8ce
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-30 09:18:41 -0300

    atividade 10

---
+++ Filas_vetor/IVetFila.java
@@ -0,0 +1,15 @@
+package Filas_vetor;
+
+public interface IVetFila {
+
+    boolean isEmpty();
+
+    boolean excluir();
+
+    boolean adiciona(Object info);
+
+    int size();
+
+    int exibir();
+
+}

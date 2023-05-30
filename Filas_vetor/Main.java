8dc8966ca56203e9943ca20b0e436705bb45f76e
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-30 09:15:53 -0300

    aivdade 10

---
+++ Filas_vetor/Main.java
@@ -0,0 +1,17 @@
+package Filas_vetor;
+
+public class Main {
+    public static void main(String[] args) {
+        VetFila f = new VetFila(8);
+        f.adiciona(4);
+        f.adiciona(5);
+        f.adiciona(6);
+        f.adiciona(7);
+        f.adiciona(8);
+        f.adiciona(9);
+        f.adiciona(3);
+        f.adiciona(1);
+        f.excluir();
+        f.exibir();
+    }
+}
---
+++ Filas_vetor/VetFila.java
@@ -0,0 +1,57 @@
+package Filas_vetor;
+
+public class VetFila {
+
+    private int nElemFila;
+    private int inicio;
+    private Object[] vetFila;
+
+    public VetFila(int tamanho) {
+        this.nElemFila = 0;
+        this.inicio = 0;
+        this.vetFila = new Object[tamanho];
+    }
+
+    public boolean isEmpty() {
+        return (nElemFila == 0);
+    }
+
+    public boolean excluir() {
+        if (this.isEmpty()) {
+            System.out.println("Fila estah vazia");
+            return false;
+        }
+        this.inicio = (this.inicio + 1) % this.vetFila.length;
+        this.nElemFila--;
+        return true;
+    }
+
+    public boolean adiciona(Object info) { // método que insere na fila
+        if (this.nElemFila == vetFila.length) {
+            System.out.println("Capacidade da fila esgotou");
+            return false;
+        }
+        int fim = (this.inicio + this.nElemFila) % this.vetFila.length;
+        this.vetFila[fim] = info;
+        this.nElemFila++;
+        return true;
+    }
+
+    public int size() {
+        return this.nElemFila;
+    }
+
+    public int exibir() {
+        if (this.isEmpty()) {
+            System.out.println("Fila estah vazia");
+            return 0;
+        }
+        int indice = this.inicio;
+        for (int i = 0; i < this.nElemFila; i++) {
+            System.out.println(this.vetFila[indice]);
+            indice = (indice + 1) % this.vetFila.length;
+        }
+        return 1;
+    }
+
+}

0668badce7328523554ff380eeaf7239de4a569e
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-05-30 09:52:27 -0300

    atividade 11

--- Fila_lde/Main.java
+++ Fila_lde/Main.java
@@ -1,5 +1,25 @@
 package Fila_lde;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        ILFila f = new LFila();
+        f.adicionar("Jose");
+        f.adicionar("Maria");
+        f.adicionar("Pedro");
+        f.adicionar("Ana");
+        f.adicionar("Leticia");
+        f.adicionar("Gabriele");
+        f.adicionar("t");
+        System.out.println("Imprimindo fila:");
+        f.exibir();
+        System.out.println("Tamanho da fila: " + f.size());
+        System.out.println("----------------------------");
+        System.out.println("Removendo elemento da fila:");
+        f.excluir();
+        f.exibir();
+        System.out.println("Tamanho da fila: " + f.size());
+
+    }
+
 }

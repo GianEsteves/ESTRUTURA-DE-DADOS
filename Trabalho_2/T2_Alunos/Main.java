9b8ea01a4d271ac31320beb2beba084e71f9868f
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-06-04 15:01:24 -0300

    organizando

---
+++ Trabalho_2/T2_Alunos/Main.java
@@ -0,0 +1,26 @@
+package T2_Alunos;
+
+public class Main {
+    public static void main(String[] args) {
+        VetPilha pilha = new VetPilha(10);
+        pilha.push("Tamires");
+        pilha.push("Gabrieel");
+        pilha.push("Maria");
+        pilha.push("Pedro");
+        pilha.push("Edurada");
+        pilha.push("Ana");
+        pilha.push("Daniellly");
+        pilha.push("Camila");
+        pilha.push("Mishelly");
+        pilha.push("Carla");
+        System.out.println("Elemntos da pilha ");
+        pilha.exibirPilha();
+        System.out.println("===============");
+        pilha.inverterPilha();
+        System.out.println("Elemntos da pilha ao contrario ");
+        pilha.exibirPilha();
+        System.out.println("Elemento do topo");
+        pilha.top();
+
+    }
+}
---
+++ Trabalho_2/T2_Alunos/VetPilha.java
@@ -0,0 +1,67 @@
+package T2_Alunos;
+
+public class VetPilha {
+    private Object[] pilha;
+    private int numElem;
+    private boolean isEmpty;
+    
+    public VetPilha(int tamanhoVetor) { // construtor
+        this.pilha = new Object[tamanhoVetor];
+        this.numElem = 0;
+    }
+    
+    public boolean push(Object info) { // empilha
+        if (this.numElem == pilha.length) {
+            System.out.println("pilha estourou");
+            return false;
+        }
+        this.pilha[this.numElem] = info;
+        this.numElem++;
+        return true;
+    }
+    
+    
+    public Object pop() { // desempilha
+        Object o = null;
+        if (this.isEmpty) {
+            System.out.println("Pilha vazia");
+            return null;
+        }
+        o = this.pilha[numElem - 1];
+        this.pilha[numElem - 1] = null;
+        this.numElem--;
+        return o;
+    }
+    
+    public boolean isEmpty() {
+        return numElem == 0;
+    }
+    
+    public void exibirPilha() { // exibe por completo 
+        if (isEmpty()) {
+            System.out.println("A pilha está vazia.");
+            return;
+        }
+        //System.out.println("Elementos da pilha:");
+        for (int i = numElem - 1; i >= 0; i--) {
+            System.out.println(pilha[i]);
+        }
+    }
+
+    public void top(){ // mostra o ultimo elemento aidiconado 
+        if (isEmpty()) {
+            System.out.println("Pilha vazia");
+            return;
+        }
+        System.out.println(pilha[numElem - 1]);
+    }
+
+    public void inverterPilha() {
+        Object[] temp = new Object[numElem];
+        for (int i = 0; i < numElem; i++) {
+            temp[i] = pilha[numElem - i - 1];
+        }
+        pilha = temp;
+    }
+    
+}
